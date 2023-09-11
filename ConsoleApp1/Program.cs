9c7d8879621347a79a589c5c81e3effd7240c967
Petar Atanasov <123156553+PEAtanasov@users.noreply.github.com>
2023-09-11 15:34:18 +0300

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,35 @@
+﻿string rowsAndColumns = Console.ReadLine();
+
+int[] rowsColsTokens = rowsAndColumns.Split(", ").Select(int.Parse).ToArray();
+
+int rows = rowsColsTokens[0];
+int cols = rowsColsTokens[1];
+
+int[,] matrix = new int[rows, cols];
+
+
+
+for (int row = 0; row < matrix.GetLength(0); row++)
+{
+    int[] input = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+
+    for (int col = 0; col < matrix.GetLength(1); col++)
+    {
+        matrix[row, col] = input[col];
+    }
+}
+
+Console.WriteLine(matrix.GetLength(0));
+Console.WriteLine(matrix.GetLength(1));
+
+int sum = 0;
+
+for (int row = 0; row < matrix.GetLength(0); row++)
+{
+    for (int col = 0; col < matrix.GetLength(1); col++)
+    {
+        sum += matrix[row, col];
+    }
+
+}
+Console.WriteLine(sum);
