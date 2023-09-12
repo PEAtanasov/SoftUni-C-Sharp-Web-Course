086768bb2d121cbd7805ad7da1c2b65ffd883ad0
Petar Atanasov <123156553+PEAtanasov@users.noreply.github.com>
2023-09-12 08:15:40 +0300

    Primary Diaogonal Matrix Lab Exersize

---
+++ PrimaryDiagonal/Program.cs
@@ -0,0 +1,30 @@
+﻿int matrixParameter = int.Parse(Console.ReadLine());
+
+
+int[,] ints = new int[matrixParameter, matrixParameter];
+
+for (int row = 0; row < ints.GetLength(0); row++)
+{
+    int[] rowInput = Console.ReadLine()
+        .Split()
+        .Select(int.Parse)
+        .ToArray();
+
+    for (int col = 0; col < ints.GetLength(1); col++)
+    {
+        ints[row, col] = rowInput[col];
+    }
+}
+int sum = 0;
+for (int r = 0; r < ints.GetLength(0); r++)
+{
+    for (int c = 0; c < ints.GetLength(1); c++)
+    {
+
+        if (r == c)
+        {
+            sum += ints[r, c];
+        }
+    }
+}
+Console.WriteLine(sum);
