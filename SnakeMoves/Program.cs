f3812ea47c30a75c77bc01ca03b04b46b477efd5
Petar Atanasov <123156553+PEAtanasov@users.noreply.github.com>
2023-09-26 19:54:42 +0300

    You are walking in the park and you encounter a snake! You are terrified and you start running zig-zag, so the snake starts following you. The program have a task to visualize the snake's path in a square form. A snake is represented by a string. The isle is a rectangular matrix of size N x M. A snake starts going down from the top-left corner and slithers its way down. The first cell is filled with the first symbol of the snake, the second cell is filled with the second symbol, etc. The snake is as long as it takes to fill the stairs– if you reach the end of the string representing the snake, start again at the beginning. After you fill the matrix with the snake's path, you should print it.

---
+++ SnakeMoves/Program.cs
@@ -0,0 +1,47 @@
+﻿string[] rowsCols = Console.ReadLine().Split();
+
+int rows = int.Parse(rowsCols[0]);
+int cols = int.Parse(rowsCols[1]);
+
+string[,] matrix = new string[rows, cols];
+
+string snake = Console.ReadLine();
+
+int startIndex = 0;
+for (int row = 0; row < matrix.GetLength(0); row++)
+{
+
+    if (row % 2 == 0) // right direction
+    {
+        for (int col = 0; col < matrix.GetLength(1); col++)
+        {
+            matrix[row, col] = snake[startIndex].ToString();
+            startIndex++;
+            if (startIndex >= snake.Length)
+            {
+                startIndex = 0;
+            }
+        }
+    }
+    else if (row % 2 != 0) // left direction
+    {
+        for (int col = matrix.GetLength(1) - 1; col >= 0; col--)
+        {
+            matrix[row, col] = snake[startIndex].ToString();
+            startIndex++;
+            if (startIndex >= snake.Length)
+            {
+                startIndex = 0;
+            }
+        }
+    }
+}
+
+for (int r = 0; r < matrix.GetLength(0); r++)
+{
+    for (int c = 0; c < matrix.GetLength(1); c++)
+    {
+        Console.Write(matrix[r, c]);
+    }
+    Console.WriteLine();
+}
