b7e2212b6a3c0584bdc51bb540bdc37ea12e7a85
Petar Atanasov <123156553+PEAtanasov@users.noreply.github.com>
2023-10-07 22:17:21 +0300

    Find largest three numbers task

---
+++ LargestThreeNumbers/Program.cs
@@ -0,0 +1,15 @@
+﻿int[] numbers = Console.ReadLine()
+    .Split()
+    .Select(int.Parse)
+    .ToArray();
+
+numbers = numbers.OrderByDescending(x => x).ToArray();
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    if (i == 3)
+    {
+        break;
+    }
+    Console.Write(numbers[i] + " ");
+}
